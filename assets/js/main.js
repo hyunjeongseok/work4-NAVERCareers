3fe8c99a4d993ca4391ddf6362dac1333c90eee0
seok <hjs031125@gmail.com>
2023-01-15 19:51:40 +0900

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@
 $(function(){
 
 
-    // header gnb
+    /**
+     * @헤더gnb마우스오버
+     * mouseenter -> 마우스가 어떤 요소 안으로 들어오는 순간 작동
+     * mouseleaver -> 마우스가 어떤 요소가 밖으로 이동하는 순간 작동
+     * .change -> 해당 셀렉값이 변할경우 변화 캐치하는 이벤트
+     */
     $('.gnb-item').mouseenter(function(){
         const child = $(this).children('ul');
         if(child.length){
@@ -18,10 +23,7 @@ $(function(){
         $(this).css('color','#000');
     })
 
-    // $('inpuxt').prop('checked',true);
-
-    // 
-
+    
     $('.btn-menu').click(function (e) { 
         e.preventDefault();
 
@@ -32,7 +34,10 @@ $(function(){
 
 
 
-    // 사이드메뉴 
+    /**
+     * @사이드메뉴열닫
+     * $(this).children() -> 상위요소의 자식
+     */
     $('.side-item').click(function (e) { 
         e.preventDefault();
         const child = $(this).children('ul');
@@ -43,7 +48,7 @@ $(function(){
     });
 
 
-    // 인트로 영상
+    
     let intro = new Swiper(".intro", {
         loop:true,
         speed: 1000,
@@ -53,8 +58,10 @@ $(function(){
     });
 
 
-
-    // mo - search Jobs
+    /**
+     * @searchJobs선택
+     * .btn-job 검색창 클릭해서 직군/직무,고용,경력 선택하기
+     */
     $('.btn-job').click(function (e) { 
         e.preventDefault();
         $(this).addClass('on');
@@ -65,7 +72,12 @@ $(function(){
         $('.pop-search').removeClass('on');
     });
 
-    // mo - search Jobs 체크박스(전체)
+
+    /**
+     *@체크박스선택및해제
+     *.prop('checked',true/false) -> '체크'되어라/되지마라
+     *.is(':checked') -> 어떠한요소를 체크해라
+     */
     $('#techall').click(function(){
         let checked = $('#techall').is(':checked');
 
@@ -104,6 +116,11 @@ $(function(){
     })
 
     // mo - search Jobs 체크박스(clear filters)
+    /**
+     * @clearFilters
+     * 체크박스 전체 해제
+     * .prop('.checked',false)
+     */
     $('.btn-clear').click(function(){
        
         $('.filter-box input:checkbox').prop('checked',false)
@@ -111,9 +128,12 @@ $(function(){
     })
 
 
-    // mo - search Jobs 메뉴 열기/닫기
-
-    // 부모요소 열기/닫기
+    /**
+     * @체크필터박스열고닫기
+     * hasClass()
+     * siblings()
+     */
+    //부모요소 열,닫
     $('.filter-box .btn-item').click(function(){
 
         if ($(this).hasClass('on')) {
@@ -125,7 +145,7 @@ $(function(){
         }
 
     })
-    // 자식요소 열기/닫기
+    //자식요소 열,닫
     $('.filter-box .btn-sub').click(function(){
 
         if ($(this).hasClass('on')) {
@@ -140,9 +160,12 @@ $(function(){
 
 
 
-
-
-    // People
+    /**
+     * @반응형swiper효과
+     * swiper-breakpoints -> 반응형에 따라 Swiper효과를 다르게 주고 싶을 때 사용
+     * breakpoints는 초기값을 모바일로 두고, 차츰 사이즈를  늘려가면 조절해야함.
+     * breakpoints안에 기입하는 숫자는 슬라이드 크기이고, 안에 적힌 값들은 브라우저가 해당 크기보다 클 떄 적용된다.
+     */
     let people = new Swiper(".people", {
         slidesPerView:1,
         spaceBetween:30,
@@ -160,9 +183,6 @@ $(function(){
 
     });
 
-
-    
-    // Benefits
     let benefits = new Swiper(".benefits", {
         slidesPerView:1,
         spaceBetween:30,
@@ -183,7 +203,10 @@ $(function(){
 
 
 
-    //footer 관련사이트
+    /**
+     * @footer사이트맵열고닫기
+     * toggleClass()
+     */
     $('.site-wrap').click(function(){
         $(this).toggleClass('active')
         if ($(this).hasClass('active')) {
